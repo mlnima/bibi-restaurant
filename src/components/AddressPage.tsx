b7f86c285091323d06aae017fb23095ef61d65e0
Nima Malayeri <mmlnima@gmail.com>
2024-06-29 11:56:22 +0200

    Address and phone number changed

--- src/components/AddressPage.tsx
+++ src/components/AddressPage.tsx
@@ -52,11 +52,13 @@ const AddressPage: FC<PropTypes> = ({activeLanguage}) => {
                     </div>
                 })}
             </div>
+            {/*<iframe*/}
+            {/*    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d201.00619512356252!2d13.285811759126597!3d52.5368601026259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a856b5a5ecb07d%3A0xbb85359b2eedd7cb!2sBibi!5e0!3m2!1sen!2sch!4v1595795729099!5m2!1sen!2sch"*/}
+            {/*    aria-hidden="false">*/}
+            {/*</iframe>*/}
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d201.00619512356252!2d13.285811759126597!3d52.5368601026259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a856b5a5ecb07d%3A0xbb85359b2eedd7cb!2sBibi!5e0!3m2!1sen!2sch!4v1595795729099!5m2!1sen!2sch"
-                aria-hidden="false">
-            </iframe>
-
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1213.4460288771138!2d13.29781360929562!3d52.53538714002195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a856b5a5ecb07d%3A0xbb85359b2eedd7cb!2sBibi%20Restaurant!5e0!3m2!1sen!2sde!4v1719654790081!5m2!1sen!2sde"
+                aria-hidden="false"></iframe>
 
         </Style>
     )
